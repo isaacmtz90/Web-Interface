61a05b5fe55581a2f74aa8d5a4225d92eb350837
isaacmtz90 <imtz90b@gmail.com>
2016-12-24 14:47:26 +0800

    Adding more concurreny benchmarks

--- services/benchmark.rb
+++ services/benchmark.rb
@@ -3,16 +3,37 @@ require 'benchmark'
 
 class Benchmark_Concurrency
   def self.cities_with_promise()
-    Benchmark.measure {
-      Concurrent::Promise.execute { results = HTTP.get("#{EventsLocatorInterface.config.WEB_API_URL}/cities") }
-    }.real
+    Benchmark.measure do
+      updates = 5.times.map do
+        Concurrent::Promise.execute { HTTP.get("#{EventsLocatorInterface.config.WEB_API_URL}/cities") }
+      end
+      updates.map { |update| update.value }
+    end.real
   end
 
-  def self.call_no_promise()
-    Benchmark.measure { results = HTTP.get("#{EventsLocatorInterface.config.WEB_API_URL}/cities")
-  }.real
+  def self.cities_no_promise()
+    Benchmark.measure do
+      5.times.map {HTTP.get("#{EventsLocatorInterface.config.WEB_API_URL}/cities")}
+    end.real
   end
+
+
+def self.events_with_promise()
+  Benchmark.measure do
+    updates = 5.times.map do
+      Concurrent::Promise.execute { HTTP.get("#{EventsLocatorInterface.config.WEB_API_URL}/city/1/events") }
+    end
+    updates.map { |update| update.value }
+  end.real
 end
+
+def self.events_no_promise()
+  Benchmark.measure do
+    5.times.map {HTTP.get("#{EventsLocatorInterface.config.WEB_API_URL}/city/1/events")}
+  end.real
+end
+end
+
 
 # HOW TO RUN:
 # => rackup Web_API
